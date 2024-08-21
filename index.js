15778c30076d44fab02d9daf00f6c54f8b4f7670
fajrul <fajrulm05@gmail.com>
2024-08-21 09:55:41 +0700

    feat: add code oop

---
+++ index.js
@@ -0,0 +1,36 @@
+class Kendaraan {
+    constructor(nama,jumlahHari) {
+        this.nama = nama;
+        this.jumlahHari =jumlahHari
+    }
+}
+
+class mobil extends Kendaraan {
+    constructor(nama,jumlahHari){
+        super(nama,jumlahHari)
+    }
+
+    hitungHargaSewa(){
+        let hargaSewa = 500000
+        let totalHarga = hargaSewa * this.jumlahHari
+        console.log(`Harga sewa mobil ${this.nama} sebesar Rp.${totalHarga.toLocaleString()}`)
+    }
+}
+
+class motor extends Kendaraan {
+    constructor(nama,jumlahHari){
+        super(nama,jumlahHari)
+    }
+
+    hitungHargaSewa(){
+        let hargaSewa = 100000
+        let totalHarga = hargaSewa * this.jumlahHari
+        console.log(`Harga sewa motor  ${this.nama}  sebesar Rp.${totalHarga.toLocaleString()}`)
+    }
+}
+
+let person1 = new mobil ("toyota",5)
+let person2 = new motor ("supra",5)
+person1.hitungHargaSewa()
+person2.hitungHargaSewa()
+
